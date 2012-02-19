4424f9d794f417ff74f6eb61c8a42d967c26c0b8
Kay Hannay <klinux@hannay.de>
2012-02-19 13:52:49 +0100

    Add install script for efaLiveSetup

---
+++ python/efaLiveSetup/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp *.py ~/entwicklung/efaLive/config/chroot_local-includes/opt/efalive/lib/efaLiveSetup/
+cp locale/de/LC_MESSAGES/efaLiveSetup.mo ~/entwicklung/efaLive/config/chroot_local-includes/opt/efalive/lib/efaLiveSetup/locale/de/LC_MESSAGES/efaLiveSetup.mo
+cp icons/* ~/entwicklung/efaLive/config/chroot_local-includes/opt/efalive/lib/efaLiveSetup/icons/
