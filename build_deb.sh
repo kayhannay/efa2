4ff9ded45de3546dc828d17948d900989e87387c
Kay Hannay <klinux@hannay.de>
2012-08-28 17:19:06 +0200

    Extract efa from efalive and move software to /usr/

---
+++ build_deb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#
+VERSION=2.0.3.5
+
+rm -r efa2-$VERSION
+mkdir efa2-$VERSION
+#cp -a content/* efa-$VERSION
+cp -a debian efa2-$VERSION
+cd content
+tar czf ../efa2_$VERSION.orig.tar.gz *
+cd ..
+cd efa2-$VERSION
+debuild -uc -us
+cd ..
+
---
+++ debian/efa.default.ex
@@ -0,0 +1,10 @@
+# Defaults for efa initscript
+# sourced by /etc/init.d/efa
+# installed at /etc/default/efa by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
