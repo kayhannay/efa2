b6df3867dc3fbef9313a3cb096695d5a48d9d7d5
Kay Hannay <klinux@hannay.de>
2013-04-25 21:59:43 +0200

    Adapt script paths; Fix install script; cleanup installation scripts

--- python/efaLiveSetup/install.sh
+++ python/efaLiveSetup/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp *.py ~/entwicklung/efaLive/config/chroot_local-includes/opt/efalive/lib/efaLiveSetup/
-cp locale/de/LC_MESSAGES/efaLiveSetup.mo ~/entwicklung/efaLive/config/chroot_local-includes/opt/efalive/lib/efaLiveSetup/locale/de/LC_MESSAGES/efaLiveSetup.mo
-cp icons/* ~/entwicklung/efaLive/config/chroot_local-includes/opt/efalive/lib/efaLiveSetup/icons/
+cp *.py ../../bash/efalive/content/usr/lib/efalive/lib/efaLiveSetup/ 
+cp locale/de/LC_MESSAGES/efaLiveSetup.mo ../../bash/efalive/content/usr/lib/efalive/lib/efaLiveSetup/locale/de/LC_MESSAGES/efaLiveSetup.mo
+cp icons/* ../../bash/efalive/content/usr/lib/efalive/lib/efaLiveSetup/icons/
