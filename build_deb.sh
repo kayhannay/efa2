d40c5c098baf5c2fc6b5d6ad0001ac79c59605af
Kay Hannay <kay.hannay@otto.de>
2021-03-09 14:55:29 +0100

    ci: use correct version in build script

--- build_deb.sh
+++ build_deb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #
-VERSION=2.2.2.54
+VERSION="2.2.2.54-1"
 CREATE_RELEASE=true
 
 if [ -f release_info.sh ]; then
@@ -13,21 +13,18 @@ if [ "${CREATE_RELEASE}" != "true" ]; then
     exit 0
 fi
 
+ORIG_VERSION=$(echo ${VERSION} | cut -d'-' -f1)
+
 rm -r efa2-*
 rm -r efa2_*
-mkdir efa2-$VERSION
-#cp -a content/* efa2-$VERSION
-cp -a debian efa2-$VERSION
+mkdir efa2-$ORIG_VERSION
+#cp -a content/* efa2-$ORIG_$VERSION
+cp -a debian efa2-$ORIG_$VERSION
 cd content
-tar czf ../efa2_$VERSION.orig.tar.gz *
+tar czf ../efa2_$ORIG_$VERSION.orig.tar.gz *
 cd ..
-cd efa2-$VERSION
 
-echo "### DEBUG ###"
-pwd
-ls -l ../
-echo "### DEBUG ###"
-
+cd efa2-$ORIG_$VERSION
 debuild -uc -us
 cd ..
 
