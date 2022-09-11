5acf7cd0e647832f542f4d53f3a94aa297b1766c
Kay Hannay <klinux@hannay.de>
2022-09-11 15:51:58 +0200

    feat: update efa to version 2.3.2_00

--- update_efa.sh
+++ update_efa.sh
@@ -2,7 +2,8 @@
 
 pushd content/usr/lib/efa2
 
-wget -q -O efa.zip http://efa.nmichael.de/download/efa230.zip && unzip -o efa.zip && rm efa.zip
+wget -q -O efa.zip http://efa.nmichael.de/download/efa232.zip && unzip -o efa.zip && rm efa.zip
+wget -q -O program/plugins/javax.mail.jar https://github.com/javaee/javamail/releases/download/JAVAMAIL-1_6_2/javax.mail.jar
 
 popd
 
